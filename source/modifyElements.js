406cd6e6a7702ab486eefc6c680ae85fca1b6265
Bloo-dev <contact.bluefire610@gmail.com>
2020-09-01 22:04:30 +0200

    Initial Commit

---
+++ source/modifyElements.js
@@ -0,0 +1,27 @@
+
+function update() {
+  document.querySelectorAll('[aria-labelledby="files"] > .js-navigation-item').forEach(e => {
+    const link = e.querySelector('[role="rowheader"] a')
+    const folder = link.getAttribute('href').slice(40)
+    if (!folder.match(/^gm4_[a-z_]+$/) || folder === 'gm4_template_pack') return
+
+    fetch(`https://raw.githubusercontent.com/Gamemode4Dev/GM4_Datapacks/master/${folder}/pack.mcmeta`)
+      .catch(err => {
+        console.log(err.message)
+      })
+      .then(res => res.json())
+      .then(data => {
+        if (data.hidden === true || data.hidden === "true") {
+          e.style.background = '#ebeef2'
+          const defaultFolderIconColor = "color-blue-3"
+          const newFolderIconColor = "color-gray-0"
+
+          var folderIcon = e.children[0].getElementsByTagName('svg')[0]
+          folderIcon.setAttribute("color",newFolderIconColor)
+          folderIcon.classList.remove(defaultFolderIconColor)
+          folderIcon.classList.add(newFolderIconColor)
+        }
+      })
+  })
+}
+update()
